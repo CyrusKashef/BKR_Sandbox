c165dafed10f28af897464d87490e181fec72eba
Cyrus Kashef <43621358+CyrusKashef@users.noreply.github.com>
2022-10-11 00:45:09 -0500

    Delete Game_Assets directory

--- Game_Assets/Speeches/Non_Furnace_Fun_Speech_Class.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-sys.path.append(".")
-from Automated.Game_Assets.Speeches.Speech_Class import SPEECH_CLASS
-
-class NON_FURNACE_FUN_SPEECH_CLASS(SPEECH_CLASS):
-    def __init__(self, file_dir, file_name):
-        super().__init__(file_dir, file_name)
